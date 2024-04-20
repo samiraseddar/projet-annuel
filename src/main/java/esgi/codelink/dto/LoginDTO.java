0003a05b6ee77ee1252e242d28ef589d7be8990e
samiraseddar <seddar20191@gmail.com>
2024-04-20 21:59:15 +0200

    add  LoginDTO

---
+++ src/main/java/esgi/codelink/dto/LoginDTO.java
@@ -0,0 +1,23 @@
+package esgi.codelink.dto;
+
+public class LoginDTO {
+    private String mail;
+    private String password;
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+

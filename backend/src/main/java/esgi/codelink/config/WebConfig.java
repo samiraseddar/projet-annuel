12d78e8dfcdf6e6a53f2006fbb0cdc22822af194
samiraseddar <121232123+samiraseddar@users.noreply.github.com>
2024-07-23 11:08:54 +0200

    Update WebConfig.java

--- backend/src/main/java/esgi/codelink/config/WebConfig.java
+++ backend/src/main/java/esgi/codelink/config/WebConfig.java
@@ -32,7 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

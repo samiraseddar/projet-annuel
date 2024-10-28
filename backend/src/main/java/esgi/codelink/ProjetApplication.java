cca24ea59592cffe0dc10918bfc7338b447f0784
chimoTN <chaimoussa.ou@gmail.com>
2024-10-28 18:08:17 +0100

    Ajout de logback

--- backend/src/main/java/esgi/codelink/ProjetApplication.java
+++ backend/src/main/java/esgi/codelink/ProjetApplication.java
@@ -1,12 +1,15 @@
 package esgi.codelink;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ProjetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetApplication.class, args);
+		log.info(" ===== RUN APPLICATION ===== ");
 	}
 }

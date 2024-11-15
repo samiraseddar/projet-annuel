daaa69460a9b9f6ac17b2314aa8b5d7f47e133c2
samiraseddar <chaimoussa.ou@gmail.com>
2024-11-15 12:14:07 +0100

    debug

--- backend/src/main/java/esgi/codelink/config/WebSocketConfig.java
+++ backend/src/main/java/esgi/codelink/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000/", "https://front-pa.vercel.app")
+                .setAllowedOrigins("http://localhost:3000/", "https://front-pa.vercel.app/")
                 .withSockJS();
     }
 
